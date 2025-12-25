a736deee4d35ba44406f2e77d5c92cf4f52c6439
Robert Pelloni <pelloni.robert@gmail.com>
2025-12-25 15:58:06 -0500

    Release v1.2.0: Batch DDC, Practice Mode, Auto-Sync Refinements

--- src/Version.h
+++ src/Version.h
@@ -1,3 +1,5 @@
 #pragma once
 
-#define ARROWVORTEX_VERSION "v1.1.0"
+// Version is now read from VERSION file at build time or manually updated here to match
+#define ARROWVORTEX_VERSION "v1.2.0"
+
